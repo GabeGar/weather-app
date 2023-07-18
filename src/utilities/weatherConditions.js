03cd09fe09dba1a045f4aed027f1c8cdd78824ea
Gabegar <gagarcia190@gmail.com>
2023-07-17 21:00:30 -0400

    imported icon imgs into via src/assets folder

--- src/utilities/weatherConditions.js
+++ src/utilities/weatherConditions.js
@@ -1,28 +1,41 @@
-const getImgSrcByCondition = (cond) => {
-    const filePath = "./icons/";
+import snow from "../assets/icons/snow.png";
+import snowShowersDay from "../assets/icons/snow-showers-day.png";
+import snowShowersNight from "../assets/icons/snow-showers-night.png";
+import thunderRain from "../assets/icons/thunder-rain.png";
+import thunderShowersDay from "../assets/icons/thunder-showers-day.png";
+import thunderShowersNight from "../assets/icons/thunder-showers-night.png";
+import rain from "../assets/icons/rain.png";
+import showersDay from "../assets/icons/showers-day.png";
+import showersNight from "../assets/icons/showers-night.png";
+import fog from "../assets/icons/fog.png";
+import wind from "../assets/icons/wind.png";
+import cloudy from "../assets/icons/cloudy.png";
+import partlyCloudyDay from "../assets/icons/partly-cloudy-day.png";
+import partlyCloudyNight from "../assets/icons/partly-cloudy-night.png";
+import clearDay from "../assets/icons/clear-day.png";
+import clearNight from "../assets/icons/clear-night.png";
 
+const getImgSrcByCondition = (cond) => {
     const conditions = {
-        snow: "snow",
-        "snow-showers-day": "snow-showers-day",
-        "snow-showers-night": "snow-showers-night",
-        "thunder-rain": "thunder-rain",
-        "thunder-showers-day": "thunder-showers-day",
-        "thunder-showers-night": "thunder-showers-night",
-        rain: "rain",
-        "showers-day": "showers-day",
-        "showers-night": "showers-night",
-        fog: "fog",
-        wind: "wind",
-        cloudy: "cloudy",
-        "partly-cloudy-day": "partly-cloudy-day",
-        "partly-cloudy-night": "partly-cloudy-night",
-        "clear-day": "clear-day",
-        "clear-night": "clear-night",
+        snow: snow,
+        "snow-showers-day": snowShowersDay,
+        "snow-showers-night": snowShowersNight,
+        "thunder-rain": thunderRain,
+        "thunder-showers-day": thunderShowersDay,
+        "thunder-showers-night": thunderShowersNight,
+        rain: rain,
+        "showers-day": showersDay,
+        "showers-night": showersNight,
+        fog: fog,
+        wind: wind,
+        cloudy: cloudy,
+        "partly-cloudy-day": partlyCloudyDay,
+        "partly-cloudy-night": partlyCloudyNight,
+        "clear-day": clearDay,
+        "clear-night": clearNight,
     };
 
-    if (cond === conditions[cond]) {
-        return `${filePath}${conditions[cond]}.png`;
-    }
+    return `${conditions[cond]}`;
 };
 
 export { getImgSrcByCondition };
--- vite.config.js
+++ vite.config.js
@@ -5,5 +5,4 @@ import react from "@vitejs/plugin-react-swc";
 export default defineConfig({
     base: "/weather-app",
     plugins: [react()],
-    publicDir: "public",
 });
